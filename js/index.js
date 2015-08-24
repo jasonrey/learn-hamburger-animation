11452e769083c569d071912a1e0ce65d01be6403
Jason Rey <jasonrey@outlook.com>
2015-08-24 09:35:18 +0800

    Reverted javascript to use vanillajs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
-$(function() {
-	$('.menu').on('click', function() {
-		$(this).toggleClass('active');
-	});
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function() {
+	var menus = document.querySelectorAll('div');
+
+	for (var i = 0; i < menus.length - 1; i++) {
+		menus[i].addEventListener('click', function(event) {
+			this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active');
+		});
+	}
 });
